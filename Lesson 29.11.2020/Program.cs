155cb47f48d81a9d43f8c06d5d57853db67609c1
gravenov <75142438+gravenov@users.noreply.github.com>
2020-11-29 19:04:27 +0300

    Создали проект

---
+++ Lesson 29.11.2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_29._11._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
